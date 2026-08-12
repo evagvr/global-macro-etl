4e67d7710089ea01a2510584ea2e37ef63702ce5
evagvr <eva.gavriliu@gmail.com>
2026-08-12 11:36:13 +0300

    Add create_tables function for macro_data, stock_prices, forex_rates

--- src/config/database.py
+++ src/config/database.py
@@ -1,6 +1,6 @@
 import psycopg2
 from contextlib import contextmanager
-from config.settings import settings
+from src.config.settings import settings
 
 @contextmanager
 def get_connection():
@@ -9,3 +9,45 @@ def get_connection():
         yield connection
     finally:
         connection.close()
+
+def create_tables():
+    with get_connection() as connection:
+        with connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS macro_data (
+                    date DATE,
+                    country TEXT,
+                    indicator_name TEXT,
+                    value NUMERIC,
+                    PRIMARY KEY (date, country, indicator_name)
+                );
+                """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS stock_prices (
+                    date DATE,
+                    symbol TEXT,
+                    country TEXT,
+                    open NUMERIC,
+                    high NUMERIC,
+                    low NUMERIC,
+                    close NUMERIC,
+                    volume NUMERIC,
+                    PRIMARY KEY (date, symbol)
+                );
+                """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS forex_rates (
+                    date DATE,
+                    currency_pair TEXT,
+                    rate NUMERIC,
+                    PRIMARY KEY (date, currency_pair)
+                )
+                """
+            )
+        
